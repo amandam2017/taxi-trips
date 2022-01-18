2169b4c324fdde963b56318e9347951865574509
amandam2017 <amandamaarman19911@gmail.com>
2022-01-18 18:09:30 +0200

    added travis build link on readme file

--- taxi-trips.js
+++ taxi-trips.js
@@ -1,3 +1,13 @@
-module.exports = function() {
+// const { Pool } = require("pg/lib")
+
+module.exports = function TaxiTrips() {
+    const totalTripCount = async(Pool)=>{
+        var trips = await pool.query
+    }
+
+
+    return{
+
+    }
 	
 }
